9f93af39b11fd8c041269184d602acdd9495d980
viranga <virangalakshanz@gmail.com>
2025-01-26 00:09:30 +0530

    setup backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+ // packages
+ 
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+ 
+
+dotenv.config();
+const port = process.env.PORT || 5300;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//app.get("/", (req, res) => {
+//  res.send("API is running....");
+//});
+
+
+
+app.use("/api/users",userRoutes);	
+
+
+
+app.listen(port, () => console.log(`Server running on port: ${port}`));
