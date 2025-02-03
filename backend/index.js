473084acf2f0f8f3621d07acbc53fa413214f08d
viranga <virangalakshanz@gmail.com>
2025-02-03 18:41:07 +0530

    Completed full backend setup for product management.

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 
 // Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js"; 
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
  
 
 dotenv.config();
@@ -30,6 +33,12 @@ app.use(cookieParser());
 
 app.use("/api/users",userRoutes);	
 app.use("/api/category",categoryRoutes);
+app.use("/api/products",productRoutes);
+app.use("/api/upload", uploadRoutes);
+
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 
 
