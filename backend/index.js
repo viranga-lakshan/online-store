27cc09630810577d87cefb200fe35ef0f8cb0f8c
viranga <virangalakshanz@gmail.com>
2025-02-04 11:57:54 +0530

    created order controller

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js"; 
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
- 
+import orderRoutes from "./routes/orderRoutes.js";
+
 
 dotenv.config();
 const port = process.env.PORT || 5300;
@@ -35,6 +36,7 @@ app.use("/api/users",userRoutes);
 app.use("/api/category",categoryRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 const __dirname = path.resolve();
